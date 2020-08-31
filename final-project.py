abf78428ed4926b68bdacdc5625ad4e28dfa63f3
Yisel Breton <yiselb41@gmail.com>
2020-08-30 20:59:30 -0400

    New bar chart method, etc

--- final-project.py
+++ final-project.py
@@ -1,4 +1,4 @@
-import pandas
+import pandas as pd
 import requests
 import io
 from sklearn.datasets.samples_generator import make_blobs
@@ -8,7 +8,7 @@ from matplotlib import style
 # NYTimes global deaths data
 url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/excess-deaths/deaths.csv'
 # load data into pandas DataFrame
-data = pandas.read_csv(io.StringIO(requests.get(url).text))
+data = pd.read_csv(io.StringIO(requests.get(url).text))
 
 # Countries organized into lists named after their respective continent
 europe = ['Austria', 'Belgium','Denmark', 'Finland', 'France', 'Germany', 'Italy',
@@ -33,6 +33,50 @@ for country in country_list:
         southam_count += int(country[8])
     elif c in asia:
         asia_count += int(country[8])
+
+euro_mean = eu_count/14
+northam_mean = northam_count/2
+southam_mean = southam_count/3
+asia_mean = asia_count/4
+
+def bar_chart(eu_count, northam_count, southam_count, asia_count, total,
+northam_mean, southam_mean, euro_mean, asia_mean):
+    '''Create bar chart
+    PARAMETERS: totals and means of data
+    '''
+    labels = ['North America', 'South America', 'Europe', 'Asia']
+    totals = [northam_count, southam_count, eu_count, asia_count]
+    means =  [northam_mean, southam_mean, euro_mean, asia_mean]
+
+    x = np.arange(len(labels))
+    width = .35
+    fig, ax = plt.subplots()
+    bars1 = ax.bar(x-width/2, means, width, label ='Means')
+    bars2 = ax.bar(x+width/2, totals, width, label='Totals')
+
+    ax.set_ylabel('Counts - in tens of millions')
+    ax.set_title('COVID-19 Total Deaths and Means by Continent')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+
+    fig.tight_layout()
+    plt.show()
+
+bar_chart(eu_count, northam_count, southam_count, asia_count, total,
+northam_mean, southam_mean, euro_mean, asia_mean)
+
+def describe_data(data):
+    """Returns statistical analysis (mean, std dev, min, etc) on data
+    PARAMETERS: CSV file
+    """
+    perc = [.25, .50, .75]  # percentiles
+    data_types = ['object', 'float64', 'int64']
+    pd.options.display.width = 0  # autodetect terminal size to print all columns
+    desc = data.describe(percentiles=perc, include=data_types)
+    print(desc)
+
+describe_data(data)
 
 def create_file(europe, northam, southam, asia, eu_count, northam_count,
 southam_count, asia_count):
@@ -67,13 +111,13 @@ with open('continentinfo.txt', 'r') as allInfo:
         print(f'{i.rstrip()}')
 
 
-def visualize(eu_count, northam_count, southam_count, asia_count):
+def kmeans_visualize(eu_count, northam_count, southam_count, asia_count):
     '''Function to visualize similarities amongst data points from NYTimes excess deaths data
     PARAMETERS: total count of deaths from Asia, Europe, South America, North America
     RETURN: uses matplotlib and sklearn to plot data points'''
 
     X,y = make_blobs(n_samples = eu_count, center_box = (100.0,10.0), cluster_std =1.0)
-    plt.scatter(X[:,0], X[:,1], s=3, color='green')
+    plt.scatter(X[:,0], X[:,1], s=3, color='green'
     X2,y2 = make_blobs(n_samples = northam_count, center_box = (100.0,10.0), cluster_std =1.0)
     plt.scatter(X2[:,0], X2[:,1], s=3, color='red')
     X3,y3 = make_blobs(n_samples = southam_count, center_box = (100.0,10.0), cluster_std =1.0)
@@ -87,4 +131,4 @@ def visualize(eu_count, northam_count, southam_count, asia_count):
     plt.show()
     plt.clf()
 
-visualize(eu_count, northam_count, southam_count, asia_count)
+# kmeans_visualize(eu_count, northam_count, southam_count, asia_count)
